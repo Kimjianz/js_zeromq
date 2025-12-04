551c668e318eb0761ed2d960eb7e8847a90022eb
Kimjianz <rlawldks@khu.ac.kr>
2025-12-05 02:26:26 +0900

    01server

---
+++ codes/lec-05-prg-01-req-rep-basic-server.js
@@ -0,0 +1,14 @@
+const zmq = require("zeromq");
+async function main(){
+    const socket = new zmq.Reply();
+    await socket.bind("tcp://*:5555");
+    
+    while(true){
+        const [message] = await socket.receive();
+        console.log("Received request:",message.toString());
+
+        await new Promise(resolve => setTimeout(resolve,1000));
+        await socket.send("world");
+    }
+
+}
