9ec7b64bcffc0bc7caba7b0e8d91cb85e0843672
Kimjianz <rlawldks@khu.ac.kr>
2025-12-06 21:32:31 +0900

    06

---
+++ codes/lec-05-prg-06-pub-sub-and-pull-push-client.js
@@ -0,0 +1,28 @@
+// lec-05-prg-06-pub-sub-and-pull-push-client.js
+
+const zmq = require("zeromq");
+
+async function main() {
+  const subscriber = new zmq.Subscriber();
+  subscriber.subscribe("");
+  subscriber.connect("tcp://localhost:5557");
+  const publisher = new zmq.Push();
+  publisher.connect("tcp://localhost:5558");
+
+  handleIncomingMessages(subscriber);
+  setInterval(async () => {
+    const rand = Math.floor(Math.random() * 100) + 1;
+    if (rand < 10) {
+      await publisher.send(String(rand));
+      console.log("I: sending message", rand);
+    }
+  }, 100);
+}
+
+async function handleIncomingMessages(subscriber) {
+  for await (const [message] of subscriber) {
+    console.log("I: received message", message.toString());
+  }
+}
+
+main();
