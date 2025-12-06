c15504e58dd482d4ed19bed920661e339a0d2f49
Kimjianz <rlawldks@khu.ac.kr>
2025-12-06 21:40:32 +0900

    07

---
+++ codes/lec-05-prg-07-pub-sub-and-pull-push-server-v2.js
@@ -0,0 +1,16 @@
+const zmq = require("zeromq");
+
+async function main() {
+  const publisher = new zmq.Publisher();
+  await publisher.bind("tcp://*:5557");
+  const collector = new zmq.Pull();
+  await collector.bind("tcp://*:5558");
+
+  while (true) {
+    const [message] = await collector.receive();
+    console.log("server: publishing update => ", message.toString());
+    await publisher.send(message);
+  }
+}
+
+main();
